b337af8c60b2a2e1ceb34ed3f3b3f67009d477ba
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-12-04 22:26:32 +0500

    Added functions for managing different user account related operations such as:  - Creating new users  - Deleting existing users  - Changing user passwords

--- main.cpp
+++ main.cpp
@@ -16,6 +16,51 @@ int executeSQL(sqlite3* db, const char* sql) {
     return 0;
 }
 
+bool checkUserExxisits(sqlite3* db, string email) {
+    string sql = "SELECT email FROM users WHERE email = '" + email + "';";
+    sqlite3_stmt* stmt;
+    sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr);
+    int result = sqlite3_step(stmt);
+    sqlite3_finalize(stmt);
+    return result == SQLITE_ROW;
+}
+
+bool createUser(sqlite3* db, string email, string name, string password, bool isAdmin) {
+    if (checkUserExxisits(db, email)) {
+        cout << "User already exists" << endl;
+        return false; // return false if user already exists
+    }
+    string sql = "INSERT INTO users (email, name, password, isAdmin) VALUES ('" + email + "', '" + name + "', '" + password + "', " + to_string(isAdmin) + ");";
+    executeSQL(db, sql.c_str());
+    return true; // return true if user is created
+}
+
+bool deleteUser(sqlite3* db, string email) {
+    if (!checkUserExxisits(db, email)) {
+        cout << "User does not exist" << endl;
+        return false; // return false if user does not exist
+    }
+    string sql = "DELETE FROM users WHERE email = '" + email + "';";
+    executeSQL(db, sql.c_str());
+    return true; // return true if user is deleted
+}
+
+bool changePassword(sqlite3* db, string email, string newPassword) {
+    if (!checkUserExxisits(db, email)) {
+        cout << "User does not exist" << endl;
+        return false; // return false if user does not exist
+    }
+    string sql = "UPDATE users SET password = '" + newPassword + "' WHERE email = '" + email + "';";
+    executeSQL(db, sql.c_str());
+    return true; // return true if password is changed
+}
+
+void defaultAdminUser(sqlite3* db) {
+    if (checkUserExxisits(db, "admin")) {
+        return;
+    }
+    createUser(db, "admin", "admin", "admin", true);
+}
 
 int main(void){
     sqlite3* db;
