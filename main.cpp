9e0d8364f41754728a8453b8618042a949a27a10
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-12-04 22:05:01 +0500

    Added code for creating the table to store users

--- main.cpp
+++ main.cpp
@@ -4,8 +4,42 @@
 
 using namespace std;
 
-int main(void){
+int executeSQL(sqlite3* db, const char* sql) {
+    char* errorMessage;
+    int exit = sqlite3_exec(db, sql, nullptr, 0, &errorMessage);
 
+    if (exit != SQLITE_OK) {
+        cerr << "Error executing SQL: " << errorMessage << endl;
+        sqlite3_free(errorMessage);
+        return 1;
+    }
+    return 0;
+}
+
+
+int main(void){
+    sqlite3* db;
+    int exit = sqlite3_open("MainDatabase.db", &db);
+    if (exit) {
+        cerr << "Error opening database: " << sqlite3_errmsg(db) << endl;
+        return -1;
+    }
+    else {
+        cout << "Database opened successfully!" << endl;
+    }
+
+    const char* create_user = R"(
+        CREATE TABLE IF NOT EXISTS users (
+        email TEXT NOT NULL UNIQUE,
+        name TEXT NOT NULL,
+        password TEXT NOT NULL
+        isAdmin BOOLEAN NOT NULL DEFAULT 0,
+        );
+    )";
+
+    if (executeSQL(db, create_user)) {
+        return -1;
+    }
 
     crow::SimpleApp studentSync;
 
